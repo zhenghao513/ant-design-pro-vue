a7625089b324da6407f5743f30cd9b809750ca72
zhenghao <zhengh513@outlook.com>
2023-06-12 22:04:02 +0800

    feat: create logo

---
+++ src/components/LoginLogo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="logo">
+    <img
+      alt="logo"
+      src="https://www.antdv.com/assets/logo.1ef800a8.svg"
+      width="32"
+    />
+    <div class="logo-text">Ant Design Pro</div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+  .logo {
+    position: fixed;
+    top: 24px;
+    left: 22px;
+    z-index: 1;
+    display: inline-flex;
+    align-items: center;
+  }
+
+  .logo-text {
+    margin-right: 4px;
+    margin-left: 4px;
+    color: #f7f8fa;
+    font-size: 20px;
+  }
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-
+    <div class="container">
+        <LoginLogo />
     </div>
 </template>
 
